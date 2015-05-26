922a0d27fe3695ece73614c2434fa024541cde6a
Troiano Andres <troianoandres@gmail.com>
2015-05-26 11:24:40 -0300

    [Troiano Andres] - Added utils

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ Builder.getTask = function (task, options, data) {
     return require(__dirname + "/src/tasks/" + task)(this.plugins.gulp, this.plugins, options, data);
 };
 
+Builder.util = function (util, options, data) {
+    return require(__direname + "/src/utils/" + utils)(this.plugins.gulp, this, options, data);
+};
+
 Builder.processOptions = function (options) {
 
 };
