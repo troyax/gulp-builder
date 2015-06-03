0d39f9f22ebc09410c79c7633f4a2ff67a62d304
Troiano Andres <troianoandres@gmail.com>
2015-06-02 22:34:27 -0300

    [Troiano Andres] - JS build info file update

--- src/tasks/js-module-build.js
+++ src/tasks/js-module-build.js
@@ -7,17 +7,16 @@ module.exports = function (gulp, options, data) {
     var uglify = require('gulp-uglify');
     var browserSync = require('browser-sync');
 
-
     return function () {
         var directory = options.modules.root + data.module;
         var buildInfo = require(directory + '/' + options.modules.buildInfoFile);
 
-        return browserify(directory + '/' + buildInfo.entry).transform(reactify).bundle()
+        return browserify(directory + '/' + buildInfo.js.directory + buildInfo.js.entry).transform(reactify).bundle()
             .on('error', function (error) {
                 console.log(error);
                 this.emit('end')
             })
-            .pipe(source( buildInfo.rename || 'app.js' ))
+            .pipe(source( buildInfo.js.rename || 'app.js' ))
             .pipe(gulpif(options.minify, buffer()))
             .pipe(gulpif(options.minify, uglify()))
             .pipe(gulp.dest( options.build.root + buildInfo.destination + 'js/' ))
