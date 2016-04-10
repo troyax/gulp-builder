0196a2410262c4c981885bc49a8d2f0677d0c408
Andres Troiano <troianoandres@gmail.com>
2016-04-10 17:01:51 -0300

    [Andres Troiano] - updated html build to generate all needed html files to allow the use of browser history

--- gulp-builder/src/tasks/build/add-html-build-task.js
+++ gulp-builder/src/tasks/build/add-html-build-task.js
@@ -2,20 +2,53 @@ var browserSync = require('browser-sync');
 var concat = require('gulp-concat');
 var handlebars = require('gulp-compile-handlebars');
 var path = require('path');
+var lodash = require('lodash');
+
+var generateBundle = function (gulp, config) {
+    var bundle = gulp.src(config.filePath).on('error', function (error) {
+        console.log(error);
+        this.emit('end');
+    });
+    var prefix = '';
+
+    lodash.times(config.depth || 0, function () {
+        prefix = path.join(prefix, '..');
+    });
+
+    if (prefix.length) {
+        prefix = prefix + '/';
+    }
+
+    bundle = bundle.pipe(handlebars({
+        prefix: prefix
+    })).pipe(concat(config.fileName || 'index.html')).pipe(gulp.dest(config.destination));
+
+    return bundle;
+};
 
 module.exports = function (gulp, config, application, taskName) {
     var destination = path.join(config.root, config.build.dir, (application.destination || ''));
     var mainFile = path.join(config.root, (application.path || ''), (application.build.html.entry || 'index.html'));
+    var routes = application.routes;
 
     gulp.task(taskName, function () {
-        var bundle = gulp.src(mainFile).on('error', function (error) {
-            console.log(error);
-            this.emit('end')
+        var bundle = generateBundle(gulp, {
+            filePath: mainFile,
+            fileName: application.build.html.rename,
+            destination: destination
         });
 
-        return bundle.pipe(handlebars({}))
-            .pipe(concat(application.build.html.rename || 'index.html'))
-            .pipe(gulp.dest(destination))
-            .pipe(browserSync.reload({stream: true}));
+        if (routes) {
+            lodash.each(routes.files, function (file) {
+                bundle = generateBundle(gulp, {
+                    filePath: mainFile,
+                    fileName: file.name,
+                    depth: file.depth,
+                    destination: path.join(destination, file.destination || '')
+                });
+            });
+        }
+
+        return bundle.pipe(browserSync.reload({stream: true}));
     });
 };
